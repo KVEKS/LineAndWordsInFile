4972970a711cef67b6a4d59359c0bbfc4fefe81f
KVEKS <aoavilov@gmail.com>
2020-04-23 19:28:45 +0300

    Clean and beauty(and try-with-resources)

--- src/LineAndWordsInFile.java
+++ src/LineAndWordsInFile.java
@@ -8,8 +8,7 @@ public class LineAndWordsInFile {
         int countLines = 0;
         int countWords = 0;
         if (!isDirectory(Paths.get(nameIn))) {
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(nameIn));
+            try (BufferedReader reader = new BufferedReader(new FileReader(nameIn))) {
                 String line = "";
                 while((line = reader.readLine()) != null) {
                     countLines++;
@@ -24,8 +23,7 @@ public class LineAndWordsInFile {
             File folder = new File(nameIn);
             File[] listOfFiles = folder.listFiles();
             for (int i = 0; i < listOfFiles.length; i++) {
-                try {
-                    BufferedReader reader = new BufferedReader(new FileReader(listOfFiles[i].getAbsolutePath()));
+                try (BufferedReader reader = new BufferedReader(new FileReader(listOfFiles[i].getAbsolutePath()))) {
                     String line = "";
                     while((line = reader.readLine()) != null) {
                         countLines++;
